df008d16dc3372f9061f0db0dc3816278eb23963
PrmdyaS <somabuwana@gmail.com>
2024-02-03 15:00:48 +0700

    crud api

---
+++ db.js
@@ -0,0 +1,18 @@
+const { MongoClient } = require('mongodb')
+let dbConnection
+let uri = 'mongodb+srv://peram24:sebenereikitesapi123@cluster0.fice8cu.mongodb.net/?retryWrites=true&w=majority'
+
+module.exports = {
+    connectToDb: (cb) => {
+        MongoClient.connect(uri)
+        .then((client) => {
+            dbConnection = client.db()
+            return cb()
+        })
+        .catch(err => {
+            console.log(err)
+            return cb(err)
+        })
+    },
+    getDb: () => dbConnection
+}
