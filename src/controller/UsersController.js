2c25594cb7778a3f9a424436a3a94919cfa27a53
PrmdyaS <somabuwana@gmail.com>
2024-04-03 19:52:59 +0700

    Edit Profile

--- src/controller/UsersController.js
+++ src/controller/UsersController.js
@@ -56,7 +56,7 @@ const getAllUsers = async (req, res) => {
 }
 
 const postUsers = async (req, res) => {
-    const { email, password, username, nama_lengkap, alamat, no_hp } = req.body;
+    const { email, password, username, nama_lengkap, alamat, no_hp, index_level_roles, profile_picture } = req.body;
 
     try {
         const existingEmail = await db.collection('user').findOne({ email });
@@ -91,6 +91,8 @@ const postUsers = async (req, res) => {
                 nama_lengkap,
                 alamat,
                 no_hp,
+                index_level_roles,
+                profile_picture,
             };
             const result = await db.collection('user').insertOne(newUser);
             const dataUser = await db.collection('user').findOne({ _id: new ObjectId(result.insertedId) });
@@ -105,20 +107,61 @@ const postUsers = async (req, res) => {
     }
 }
 
-const getOneUsers = (req, res) => {
+const getOneUsers = async (req, res) => {
     if (ObjectId.isValid(req.params.id)) {
-        db.collection('user')
-            .findOne({ _id: new ObjectId(req.params.id) })
-            .then(doc => {
-                res.status(200).json({
-                    message: "success",
-                    status: 200,
-                    data: doc
-                })
-            })
-            .catch(err => {
-                res.status(500).json({ error: 'Could not fetch the document' })
-            })
+            try {
+                const cursor = db.collection('user').aggregate([
+                    {
+                        $match: { _id: new ObjectId(req.params.id) }
+                    },
+                    {
+                        $lookup: {
+                            from: 'roles',
+                            as: 'roles',
+                            let: { indexLevelRoles: "$index_level_roles" },
+                            pipeline: [
+                                {
+                                    $match: {
+                                        $expr: {
+                                            $and: [
+                                                { $eq: ["$index_level", "$$indexLevelRoles"] }
+                                            ]
+                                        }
+                                    }
+                                },
+                                {
+                                    $project: {
+                                        index_level: 0
+                                    }
+                                }
+                            ]
+                        }
+                    },
+                    {
+                        $set: {
+                            roles: { $arrayElemAt: ["$roles", 0] }
+                        }
+                    },
+                    {
+                        $project: {
+                            email: 0,
+                            password: 0,
+                            index_level_roles: 0
+                        }
+                    }
+                ]);
+                const users = await cursor.next();
+        
+                if (users) {
+                    res.status(200).json({
+                        message: "success",
+                        status: 200,
+                        data: users
+                    });
+                }
+            } catch (error) {
+                res.status(500).json({ error: 'Could not find the document' });
+            }
     } else {
         res.status(500).json({ error: 'Not a valid document id' })
     }
